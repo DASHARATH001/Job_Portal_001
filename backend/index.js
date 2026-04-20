aa668f8c1a93bca8e775dc5017f7b5d1f98f7071
dasharathkumar2 <dasharathkumar2403@gmail.com>
2026-04-20 18:04:11 +0530

    allow origin

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 8080;
 // ✅ CORS (ONLY ONCE, NO STAR)
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://job-portal-001-navy.vercel.app",
     credentials: true,
   })
 );

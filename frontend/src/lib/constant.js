8dcd841364e4433ba5e1d98f1be8f20025f9f8df
dasharathkumar2 <dasharathkumar2403@gmail.com>
2026-04-20 17:55:01 +0530

    chnage uri

--- frontend/src/lib/constant.js
+++ frontend/src/lib/constant.js
@@ -1,5 +1,5 @@
-export const USER_API_END_POINT = "http://localhost:8080/api/user";
-export const JOB_API_END_POINT = "http://localhost:8080/api/job";
-export const APPLICATION_API_END_POINT = "http://localhost:8080/api/application";
-export const COMPANY_API_END_POINT = "http://localhost:8080/api/company";
+export const USER_API_END_POINT = "https://job-portal-001.onrender.com/api/user";
+export const JOB_API_END_POINT = "https://job-portal-001.onrender.com/api/job";
+export const APPLICATION_API_END_POINT = "https://job-portal-001.onrender.com/api/application";
+export const COMPANY_API_END_POINT = "https://job-portal-001.onrender.com/api/company";
 
